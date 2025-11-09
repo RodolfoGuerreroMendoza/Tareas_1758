0abdaea4fc975d9c40fe66dc9b3bba8e2bfd11a9
RodolfoGuerreroMendoza <rodolfoguerrero25@aragon.unam.mx>
2025-11-09 15:02:46 -0600

    commit prueba

---
+++ app.js
@@ -0,0 +1,10 @@
+//Arreglo
+//              0           1         2         3
+let frutas = ["manzana", "pera", "platano", "fresa"];
+console.log(frutas);
+
+console.log(frutas.length);
+console.log(frutas[0]);
+console.log(frutas[1]);
+console.log(frutas[5]); //undefined
+
